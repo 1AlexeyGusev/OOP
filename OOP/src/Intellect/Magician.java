cb64d62fbee136aa06f449eead4e1ef56254ad89
AlexG <gusev_alexey@mail.ru>
2024-02-17 23:34:46 +0300

    изменена ветка

--- OOP/src/Intellect/Magician.java
+++
@@ -1,9 +0,0 @@
-package Intellect;
-
-public class Magician extends Intellect {
-    int mana;
-    int maxMana;
-    public Magician(String name, int HP, int maxHP, int attack, int attackRange, int defend, int initiative, int level, int experience, int intellect, int magic, int maxMagic) {
-        super(name, HP, maxHP, attack, attackRange, defend, initiative, level, experience, intellect, magic, maxMagic);
-    }
-}
